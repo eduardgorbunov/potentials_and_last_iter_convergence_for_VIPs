2ce310b42569a142575b1d4ca92e51658389ee4f
eduardgorbunov <ed-gorbunov@yandex.ru>
2022-02-16 13:01:34 +0300

    initial

---
+++ Optimistic_Gradient/EG_FTP_norm_analysis.m
@@ -0,0 +1,61 @@
+clear all; clc;
+%
+% In this example, we consider Optimistic gradient method:
+% w_{k+1} = w_k - 2 * gamma * F(w_k) + gamma * F(w_{k-1})
+
+% (0) Initialize an empty PEP
+P=pep();
+
+N = 2; %number of iterations
+
+% (1) Set up the class of monotone inclusions
+param.L  =  1; param.mu = 0; % F is 1-Lipschitz and 0-strongly monotone
+%param.beta = 1.0;
+
+
+
+gamma = 1/(4*param.L);
+%gamma = param.beta/4;
+
+F = P.DeclareFunction('LipschitzStronglyMonotone',param);
+%F = P.DeclareFunction('Cocoercive',param);
+
+% (2) Set up the starting points
+w0=P.StartingPoint();
+[ws, Fs] = F.OptimalPoint(); 
+
+P.InitialCondition((ws-w0)^2<=1);  % Normalize the initial distance ||w0-ws||^2 <= 1
+
+% (3) Algorithm
+
+w = cell(N+1,1);
+tw = cell(N+1,1);
+w{1} = w0;
+tw{1} = w0;
+%w{2} = w{1} - gamma * F.evaluate(tw{1});
+for i = 1:N
+    tw{i+1} = w{i} - gamma * F.evaluate(tw{i});
+    w{i+1} = w{i} - gamma * F.evaluate(tw{i+1});
+end
+
+% (4) Set up the performance measure: ||F(w_k)||^2
+squared_norm = (F.evaluate(w{N+1}))^2;
+norm_F_prev = (F.evaluate(w{N}))^2;
+P.PerformanceMetric(squared_norm - norm_F_prev);
+
+% (5) Solve the PEP
+P.solve()
+
+% (6) Evaluate the output
+double(squared_norm - norm_F_prev)   % worst-case squared norm
+
+res_norm = double(squared_norm);
+res_init_dist = double((ws-w0)^2);
+    
+%save(strcat('dump/OG_norm_L_1_N_', sprintf('%d_', N), sprintf('_%f', gamma),'.mat'), 'res_norm', 'gamma', 'res_init_dist');
+
+fprintf("======================================================\n");
+fprintf("N = %d: ", N);
+fprintf("||F(x^N)||^2 = %f, ||x^0 - x^*||^2 = %f\n", double(squared_norm), res_init_dist);
+fprintf("||F(x^{N-1})||^2 = %f\n", double(norm_F_prev));
+fprintf("======================================================\n");
